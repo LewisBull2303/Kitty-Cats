a82e08b6e8f1c9ef57f7a7660c0830af2f7bba2a
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2024-08-20 17:09:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.addEventListener("click", function (event) {
+    if(!event.target.matches("#button")) return;
+
+    fetch("https://api.thecatapi.com/v1/images/search")
+    .then ((response) => response.json())
+    .then((data) => renderCat(data))
+
+    fetch ("https://meowfacts.herokuapp.com/")
+    .then ((response) => response.json())
+    .then ((data) => catFact(data))
+});
+
+function renderCat (data){
+    const catPhoto = document.getElementById("kittyCat");
+    catPhoto.setAttribute("src", data[0].url);
+}
+
+function catFact (data){
+    const kittyFact = document.getElementById("fact");
+    console.log(data["data"])
+    kittyFact.innerHTML = data["data"];
+}
